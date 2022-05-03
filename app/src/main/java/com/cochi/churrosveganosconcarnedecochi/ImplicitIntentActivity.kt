92e84be9acad229a1da37a766ae5047474a29cf2
MajoLL03 <maria.lopez.lugo.2019@cetis108.edu.mx>
2022-05-03 13:24:10 -0600

    Intent implicitos
    - Definir la acción para intent de tipo envio de email

--- app/src/main/java/com/cochi/churrosveganosconcarnedecochi/ImplicitIntentActivity.kt
+++ app/src/main/java/com/cochi/churrosveganosconcarnedecochi/ImplicitIntentActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
@@ -14,7 +15,33 @@ class ImplicitIntentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_implicit_intent)
 
         agregarAccionAlBotonWeb()
+        agregarAccionAlBotonEmail()
     }
+
+    private fun agregarAccionAlBotonEmail() {
+        // obtener el enlace al botonEmail
+        val botonCorreo = findViewById<Button>(R.id.buttonEmailMe)
+        //asignar la acción al botonCorreo para que envíe un mensaje por correo
+        botonCorreo.setOnClickListener {
+            // definir al remitente
+            val email = "maria@lopez.com"
+            // inicializar el intent con el remitente
+            val intentEmail = Intent(Intent.ACTION_SEND, Uri.parse(email))
+            // agrega el asunto al correo
+            intentEmail.putExtra(Intent.EXTRA_SUBJECT, "Hola desde android")
+            // Agregar el mensaje al correo
+            intentEmail.putExtra(Intent.EXTRA_TEXT, "Responde por favor no te hagas cochi")
+            // agregar el destinatario
+            intentEmail.putExtra(Intent.EXTRA_EMAIL, "convenencieros@del.sexto.a.matutino")
+            // definir el tipo de contenido
+            intentEmail.type = "text/plain"
+            // ejecutar el intent
+            startActivity(Intent.createChooser(intentEmail, "Enviar con..."))
+
+        }
+    }
+
+
 
     private fun agregarAccionAlBotonWeb() {
             // obtener el enlace al boton para enviar uri a web
